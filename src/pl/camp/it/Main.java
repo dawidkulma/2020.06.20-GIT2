02b71b4830711357d21a6094b88adccd82791952
Dawid Kulma <dawidkulma@gmail.com>
2020-06-20 13:30:50 +0200

    koM

--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -1,9 +1,12 @@
 package pl.camp.it;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("GIT2");
         System.out.println("GIT3");
+        System.out.println("GIT4");
     }
 }
