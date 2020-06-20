c885df81753b84ee7a872ff050eea09c531cd4e8
Dawid Kulma <dawidkulma@gmail.com>
2020-06-20 12:36:57 +0200

    KOM2

--- src/pl/camp/it/Main.java
+++ src/pl/camp/it/Main.java
@@ -3,5 +3,7 @@ package pl.camp.it;
 public class Main {
     public static void main(String[] args) {
         System.out.println("GIT");
+        System.out.println("GIT2");
+        System.out.println("GIT3");
     }
 }
